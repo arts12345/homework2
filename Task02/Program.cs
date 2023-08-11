6987bde9f07b32fe4742df165bf8f5fe788e0e9a
tryforeese <stas.desu@yandex.ru>
2023-08-11 16:18:21 +0300

    Решение первой задачи

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число: ");
+        int number =Convert.ToInt32(Console.ReadLine());
+
+       void SecondRange(int num)
+        {
+            if (num >= 100 && num <= 999)
+            {
+                Console.WriteLine(num%100/10);
+            }
+            else Console.WriteLine("Число должно быть трёхзначным");
+        }
+        SecondRange(number);
