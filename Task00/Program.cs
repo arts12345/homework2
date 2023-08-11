760a521206e23f1a02a9e5600ea26c8a4fad25a7
tryforeese <stas.desu@yandex.ru>
2023-08-11 16:02:43 +0300

    Решение второй задачи

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.WriteLine("введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        bool ValidateNumb( int num)
+        {
+            if (num > 100)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        int num999 ( int number)
+        {
+            while (number > 999) 
+            {
+                number /= 10;
+            }
+            return number % 10;
+        }
+
+        if (ValidateNumb(num))
+        {
+            Console.WriteLine(num999(num));
+        }
